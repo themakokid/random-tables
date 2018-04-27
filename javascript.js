2eee89e3756db26d66ea042a8c401046ddc854b4
themakokid <dpatino@outlook.com>
2018-04-27 14:49:48 -0600

    1

---
+++ javascript.js
@@ -0,0 +1,11 @@
+var randoms = [
+  'Random1',
+  'Random2',
+  'Random3',
+  'Random4'
+]
+
+function rando() {
+  var randomNumber = Math.floor(Math.random() * (randoms.length);
+  document.getElementbyId('randoDisplay').innerHTML = randoms[randomNumber];
+}
