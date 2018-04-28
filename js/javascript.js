ddb9dcf1f17325202af0dd0e43eda721c7d8910b
themakokid <dpatino@outlook.com>
2018-04-28 00:26:19 -0600

    d

--- js/javascript.js
+++ js/javascript.js
@@ -1,11 +1,9 @@
 function genlocation() {
-
   var randomNumber = Math.floor(Math.random() * locations.length);
   document.getElementById('locationsDisplay').innerHTML = "You find:" + locations[randomNumber];
 }
 
 function genlandmark() {
-
   var randomNumber = Math.floor(Math.random() * landmarks.length);
   document.getElementById('landmarksDisplay').innerHTML = "You see:" + landmarks[randomNumber];
 }
